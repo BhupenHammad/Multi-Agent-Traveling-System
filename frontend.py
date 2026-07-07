096b49a9d242bc9113f424a425bbfd6f63511723
BhupenHammad <bhupenhammad@gmail.com>
2026-07-07 09:20:36 +0530

    deploy

--- frontend.py
+++ frontend.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from datetime import datetime
 from langchain_core.messages import HumanMessage
-from app import app
+from main import app
 
 st.set_page_config(
     page_title="AI Travel Booking System",
